e490392103271ff9a7261f969b257e69737d8966
Marco Bruno <marco.bruno.br@gmail.com>
2017-05-15 15:04:08 -0300

    Minor

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const Koa = require('koa')
 const app = new Koa()
 const port = 3000
 
-app.use(ctx => ctx.body = 'Hello World')
+app.use(ctx => ctx.body = 'Olás')
 
 app.listen(port, () => {
   console.log(`Servidor criado em: http://localhost:${port}`)
