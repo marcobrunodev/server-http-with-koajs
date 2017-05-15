546b471386f1c701432f77d60bcc95911dda4d75
Marco Bruno <marco.bruno.br@gmail.com>
2017-05-15 19:09:56 -0300

    Added the koa-router with dependencie

--- app/server.js
+++ app/server.js
@@ -1,8 +1,34 @@
 const Koa = require('koa')
+const Router = require('koa-router')
+
 const app = new Koa()
+const router = new Router()
 const port = 3000
 
-app.use(ctx => ctx.body = 'Olás')
+router
+  .get('/', (ctx, next) => {
+    ctx.body = '<h1>Você está na Home!</h1>'
+  })
+  .get('/contato', (ctx, next) => {
+    ctx.body = (`
+      <h1>Contato</h1>
+
+      <form action="/contato" method="POST">
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email">
+
+        <label for="mensagem">Mensagem:</label>
+        <textarea name="mensagem" id="mensagem"></textarea>
+
+        <input type="submit" value="Enviar">
+      </form>
+    `)
+  })
+  .post('/contato', (ctx, next) => {
+    ctx.body = '<h1>Precisamos aprender a pegar os valores que o usuário digitou!</h1>'
+  })
+
+app.use(router.routes())
 
 app.listen(port, () => {
   console.log(`Servidor criado em: http://localhost:${port}`)
